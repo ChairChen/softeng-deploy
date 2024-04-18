e3f5df41fdfcc2fbbcc9a9b975ff69a0cc427901
ChairChen <benbeneeg@gmail.com>
2024-04-18 18:59:41 -0500

    Updated Web Title

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from software engineering</h1>
-        <h3>Student I-Zhi Chen, Homework06</h3>
+        <h1>Hello from Student I-Zhi Chen</h1>
+        <h3>Software Engineering Homework06</h3>
       </header>
     </div>
   );
