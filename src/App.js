ccda13b54315097520d44a0ab6a71f9a0d547c66
ChairChen <benbeneeg@gmail.com>
2024-04-18 18:06:34 -0500

    Created a React JS project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from software engineering</h1>
+        <h3>Student I-Zhi Chen, Homework06</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
